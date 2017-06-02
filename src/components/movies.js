1eb71a1a92e36de1e3a4c586cec679dd7d04ea72
Polina Zolotukhina <polina@iwoork.com>
2017-06-02 10:22:22 +0100

    CSS: Change the height of movie

--- src/components/movies.js
+++ src/components/movies.js
@@ -10,25 +10,27 @@ export default class MoviesSearch extends Component {
         <div className="row">
             {
               movieprop && movieprop.slice(0,6).map((item ) =>
-                <div className=" col-md-4" key={item.id}>
-                    <a href={"https://www.google.co.uk/search?q=" + item.title }>
-                      {
-                        item.backdrop_path ? (
-                          <img className="moveImg"
-                          onClick={this.showInfoOnClick}
-                          src={"http://image.tmdb.org/t/p/w342" + item.backdrop_path} />
-                        ) : (
-                          <img className="moveImg" src="https://www.omao.noaa.gov/sites/all/themes/noaa_omao/images/video-placeholder-640.jpg" />
-                        )
-                      }
-                    </a>
-                    <div className="info">
-                        <a href={"https://www.google.co.uk/search?q=" + item.title }>
-                            <h4 className="movieName">{item.title }</h4>
-                        </a>
-                        <p>Release date : {item.release_date}</p>
-                        <p>👍🏻{item.vote_average}</p>
-                    </div>
+                <div className=" col-md-4 sm-12" key={item.id}>
+                  <div className="movie">
+                      <a href={"https://www.google.co.uk/search?q=" + item.title }>
+                        {
+                          item.backdrop_path ? (
+                            <img className="moveImg"
+                            onClick={this.showInfoOnClick}
+                            src={"http://image.tmdb.org/t/p/w342" + item.backdrop_path} />
+                          ) : (
+                            <img className="moveImg" src="https://www.omao.noaa.gov/sites/all/themes/noaa_omao/images/video-placeholder-640.jpg" />
+                          )
+                        }
+                      </a>
+                      <div className="info">
+                          <a href={"https://www.google.co.uk/search?q=" + item.title }>
+                              <h4 className="movieName">{item.title }</h4>
+                          </a>
+                          <p>Release date : {item.release_date}</p>
+                          <p>👍🏻{item.vote_average}</p>
+                      </div>
+                  </div>
                 </div>
               )
             }
